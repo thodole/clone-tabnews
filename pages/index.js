beebb251e55ece3a57fc826bd6f29b620700b2d8
Glaudervane de Moura <thodole@gmail.com>
2024-02-03 19:57:08 +0000

    inclui/testa  +html & nova url

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function Home() {
-    return <h1>Pai, realiza o meu pedido!?</h1>
+    return  <html>
+            <h1>Pai, realiza o meu pedido!?</h1>
+            <h2>Ficarei muito contente e agradecido.</h2>
+            </html>
 }
 
 export default Home;
