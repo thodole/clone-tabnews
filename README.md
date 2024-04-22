e1377e8170e05f284b14f76b3d3a605e684748ea
Glauder Moura <thodole@gmail.com>
2024-04-22 12:42:29 +0000

    modifica a frase

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
-Implementação do https://www.tabnews.com.br para o https://curso.dev Aprendendo a fazer um projeto do zero!
+Implementação do https://www.tabnews.com.br para o https://curso.dev
+Aprendendo a fazer um projeto do zero!
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 function Home() {
-    return  <html>
-            <h1>Pai, realiza o meu pedido!?</h1>
+    return (
+        <>
+            <h1>Pai, atende ao meu pedido?</h1>
             <h2>Ficarei muito contente e agradecido.</h2>
-            </html>
+        </>
+    )
 }
 
 export default Home;
