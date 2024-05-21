6feab21180f1748b621bbab85a4cc000f8e0691b
Glauder Moura <thodole@gmail.com>
2024-05-21 10:58:31 +0000

    atualiza a frase, como treino.

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home() {
     return (
         <>
             <h1>Pai, atende ao meu pedido?</h1>
-            <h2>Ficarei muito contente e agradecido.</h2>
+            <h2>Ficarei muito contente e grato.</h2>
         </>
     )
 }
